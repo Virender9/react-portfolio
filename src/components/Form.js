b536929ef9c623138a334cb51fab865dd22d9e3d
Virender9 <r.virenderverma1@gmail.com>
2022-05-23 20:39:04 +0530

    added emailjs for contact form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./styles/FormContact.css";
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gi95fh4', 'template_qk2v05t', form.current, 'RLGuUO6r3LoIaIB4w')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      alert("Email Sent!");
+      };
+
+
   return (
     <div className='form'>
-      <form>
+      <form ref={form} onSubmit={sendEmail}> 
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" name="user_name"></input>
         <label>Email</label>
-        <input type="email"></input>
+        <input type="email" name="user_email"></input>
         <label>Subject</label>
-        <input type="text"></input>
+        <input type="text" name="subject"></input>
         <label>Message</label>
-        <textarea rows="6" placeholder="Enter Your Message" />
-        <button className='btn'>Submit</button>
+        <textarea rows="6" placeholder="Enter Your Message" name="message"/>
+        <button className='btn' value="send">Submit</button>
       </form>
     </div>
   )
